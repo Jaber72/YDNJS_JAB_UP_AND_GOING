74a33da19bb225e1468065bf1e8bfe0e90c3d775
Bergstedt <jeffrey.bergstedt@siemens.com>
2018-12-02 15:55:42 -0600

    test

--- Exercises/YDNJSV1_P026_Ex01.js
+++ Exercises/YDNJSV1_P026_Ex01.js
@@ -3,6 +3,10 @@
 const START_BALANCE = 1000.00;
 const MIN_BALANCE_TOLERATED = 10;
 var balance = START_BALANCE;
+var i = 0;
+var costs = [300, 20, 250, 15, 300, 40, 1000, 20];
+var totalCost = 0.0;
+var success = true;
 
 function getCost() {
 
@@ -22,10 +26,12 @@ function purchase(cost) {
 function calcTax(totalCost) {
 return totalCost*.05
 }
-var i = 0;
-var costs = [300, 20, 250, 15, 300, 40, 1000, 20];
-var totalCost = 0.0;
-var success = true;
+
+function formatMoney(amount) {
+  var formattedAmount = amount.toString();
+  formattedAmount = "$" + formattedAmount;
+  return formattedAmount;
+}
 
 while( balance > MIN_BALANCE_TOLERATED && i < costs.length ) {
   // var cost = getCost();
@@ -43,3 +49,4 @@ while( balance > MIN_BALANCE_TOLERATED && i < costs.length ) {
   console.log( "TotalCost " + totalCost + " \n Out Loop Balance remaining: " + balance + "\n\n" );
   totalCost = totalCost + calcTax( totalCost );
   console.log( "\n Out Loop TotalCost with tax: " + totalCost.toFixed(2) );
+  console.log("Final total cost with formatting = " + formatMoney(totalCost) + ".")
